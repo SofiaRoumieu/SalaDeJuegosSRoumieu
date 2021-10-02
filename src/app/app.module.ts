872828a26af4d0498e65ebea6f94cbe3b4c7a0cc
SofiaRoumieu <roumieusofia@gmail.com>
2021-10-02 13:01:09 -0300

    Primer commit. Modificaciones para deploy heroku

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { MenuPrincipalComponent } from './partes/menu-principal/menu-principal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ErrorComponent,
+    MenuPrincipalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
